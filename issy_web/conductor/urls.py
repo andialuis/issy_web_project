04d33c5717888d26232ee25a7a42cdb7231d3141
Sebastian Wilde Alarcon Arenas <sebastian.alarcon@ucsp.edu.pe>
2017-10-26 10:19:17 -0500

    Front end info conductor

--- issy_web/conductor/urls.py
+++ issy_web/conductor/urls.py
@@ -6,6 +6,7 @@ from conductor import views
 urlpatterns = [
 url(r'^conductor/$',views.AlquilarAuto.as_view(),name="alquilar_auto"),
 url(r'^conductor/lista_autos$',views.alquilar_auto_lista,name="lista_autos"),
+url(r'^conductor/conductor_info$',views.ConductorInfo.as_view(),name="conductor_info"),
 url(r'^$',views.AlquilarAuto.as_view(),name="alquilar_auto"),
 
 ]
